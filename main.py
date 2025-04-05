5fe55a43733b38eff78494facd29875569201c78
edrinthomas24 <157085349+edrinthomas24@users.noreply.github.com>
2025-04-05 16:52:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from utils.quiz_utils import init_db
+
+# Initialize DB on first run
+init_db()
+
+st.set_page_config(page_title="Smart StudyMate", layout="centered")
+st.title("📚 Smart StudyMate")
+st.markdown("AI-powered Study Assistant to help you learn better!")
+
+st.sidebar.success("Select a page from the left 👈")
 
